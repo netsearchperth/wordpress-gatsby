2e04ada17de6e9dc47296fe738651d4331440beb
netsearchperth <82921360+netsearchperth@users.noreply.github.com>
2021-04-21 11:00:24 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       options: {
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://dev-gatsby-source-wordpress-v4.pantheonsite.io/graphql`,
+          `http://staging.netsearch.com.au/ohhenryvintage/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: true,
